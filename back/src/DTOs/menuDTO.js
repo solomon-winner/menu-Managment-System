57c6dd6df63549852d36578c11dabfe8233e709e
solomon-winner <solyalew3@gmail.com>
2024-10-08 10:48:35 +0300

    ...menu DTO...

---
+++ back/src/DTOs/menuDTO.js
@@ -0,0 +1,23 @@
+class MenuDTO {
+    constructor({ id, name, parentId, children }) {
+      this.id = id;
+      this.name = name;
+      this.parentId = parentId;
+      this.children = children;
+    }
+  
+    static fromMenuItem(menuItem) {
+      return new MenuDTO({
+        id: menuItem._id,
+        name: menuItem.name,
+        parentId: menuItem.parentId,
+        children: menuItem.children,
+      });
+    }
+  
+    static fromMenuItems(menuItems) {
+      return menuItems.map(menuItem => MenuDTO.fromMenuItem(menuItem));
+    }
+  }
+  
+  export default MenuDTO;
